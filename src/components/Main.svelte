07ede1177a0ec79fe7b161ba39e709f1fc4c0316
Yulian Borges Lobaina <yborges2005@gmail.com>
2024-12-12 11:01:45 +0100

    añadido el NavBar a Main

--- src/components/Main.svelte
+++ src/components/Main.svelte
@@ -1,19 +1,23 @@
 <script>
-    // Puedes añadir lógica o props si es necesario
-  </script>
-  
-  <style>
+    import NavBar from "./NavBar.svelte";
+    let navOptions = ["Inicio", "Perfil", "Configuración", "Salir"];
+
+</script>
+
+<main>
+    <NavBar options={navOptions} />
+    <div class="content">
+        <p>Contenido Principal</p>
+    </div>
+</main>
+
+<style>
     main {
-      flex-grow: 1; /* Ocupa el espacio restante */
-      width: 100%;
-      background-color: #ffffff; /* Cambia a tu color preferido */
-      display: flex;
-      align-items: center;
-      justify-content: center;
+        flex-grow: 1; /* Ocupa el espacio restante */
+        width: 100%;
+        background-color: #ffccb3; /* Cambia a tu color preferido */
+        display: flex;
+        align-items: center;
+        justify-content: center;
     }
-  </style>
-  
-  <main>
-    <p>Contenido Principal</p>
-  </main>
-  
+</style>
