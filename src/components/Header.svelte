d050517cc4d3913e0f58dbf5de231c8a53969f5d
Yulian Borges Lobaina <yborges2005@gmail.com>
2024-12-12 10:36:20 +0100

    cambiado el color de fondo y añadia sombra al Header

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -6,11 +6,12 @@
     header {
       height: 16%;
       width: 100%;
-      background-color: #f8f9fa; /* Cambia a tu color preferido */
+      background-color: #FF8A66; /* Cambia a tu color preferido */
       display: flex;
       align-items: center;
       justify-content: center;
       text-align: center;
+      box-shadow: 0px 4px 6px rgba(255, 138, 102, 0.5); /* Sombra */
     }
   </style>
   
